b2ab8d2e007e69b93b7de5927837ceaeefc86e81
annum25 <alfredannum26@gmail.com>
2025-11-10 10:57:41 +0000

    updated

---
+++ Largest number.py
@@ -0,0 +1,8 @@
+#start
+number_1 = float(input("Enter the first number = "))
+number_2 = float(input("Enter the second number = "))
+number_3 = float(input("Enter the third number = "))
+#compare the numbers
+largest = max(number_1, number_2, number_3)
+#display
+print(f"The largest number is =", largest)
